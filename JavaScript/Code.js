053d37650d2cc52881f06cfc31b9fbbd27a2c726
rezig rachid <117665898+rrezi69140@users.noreply.github.com>
2022-12-06 17:10:12 +0100

    importation de la vaersion envoyer au prof

--- JavaScript/Code.js
+++ JavaScript/Code.js
@@ -25,6 +25,37 @@ else{
 }
    
 }
+function ConexionBis() {
+    var Utilisateur = prompt("Entrer le nom d'utilisateur : ") // creation de la variable utilisateur
+    var Mdp = prompt("Entrer votre Mot de passe: ")//creation de la variable du mot de passe
+    if (Utilisateur ==="Admin" && Mdp === "Root"){ // si l'utilisateur est egale a admin et le mdp est egale a root 
+        window.open("PageAdmin/indexAdmin.html")// alors redirection vesr la page d'aceuile admin
+        
+        
+    
+    }
+    else if  (Utilisateur ==="Admin"){ // si seul l'utilisatuer est bon 
+        alert("Mot de passe eronner")//alors afficher mdp erroner
+    }
+    else if  (Mdp ==="Root"){ // si seul mdp erronner
+        alert("Utilisateur  eronner")  // alors afficher  utilisateur erroner             
+    }
+        
+    else{
+        alert("Utilisateur et MDP  erroner") // sinon afficher utilisateur et mdp faux
+    }
+       
+    }
+
+
+function Deconexion() {
+    var Saisie = prompt("Voulez vous vous deconectez ? Oui/Non ") // creation de la variable utilisateur
+    if (Saisie ==="Oui" ){ // si l'utilisateur est egale a admin et le mdp est egale a root 
+        window.open("../../index.html")// alors redirection vesr la page d'aceuile admin
+     
+    }
+}
+    
 
 
 var  TableauPrenomMembre = new Array();// creation du tableau ou seron stocker les membres
@@ -38,13 +69,13 @@ var TableIdMembre = new Array();// creation du tableau ou seron stocker les memb
 function CreationMembre(){ // creation de la donction qui va gerer la creation d'un nouveau membre
 
 
-    var CreationMembre = prompt("voulez vous crée un nouveau memrbe (Oui/Non) : "); // demande de creation de mmembre
+    var CreationMembre = prompt("voulez vous crée un nouveau membre (Oui/Non) : "); // demande de creation de mmembre
     
 
     if (CreationMembre = "Oui") {
         var IdMembre = 0; // initialisation de la variable ID
         var Indice = 0 // initialisation de la variable Indice
-        var NombreMembre = parseInt(prompt("Combient de mmbre voulez vous crée"));// demande du nombre de mmebre a crée
+        var NombreMembre = parseInt(prompt("Combient de membre voulez vous crée"));// demande du nombre de mmebre a crée
         
         for (Indice; Indice < NombreMembre;Indice++){ // Tant que l'indice est plus petit que le nombres de membre que l'on veut entrer on repete la boucle de saisie et de contage des membres
             IdMembre = IdMembre+1;
@@ -55,11 +86,11 @@ function CreationMembre(){ // creation de la donction qui va gerer la creation d
             
                           
                           if( Majoriter >= 18) {
-                                  majeur = "oui";             // verification de l'age
+                                  majeur = "Oui";             // verification de l'age
                           }
 
                           else {
-                                  majeur = "non";
+                                  majeur = "Non";
                           }
 
 
@@ -126,9 +157,82 @@ function FonctionRechercheAdmin() {
     
     let input = document.getElementById('BareRecherche').value
     input=input.toLowerCase();
-    window.open(input+".html")
+    window.open(input+"Admin.html")
 }
 
 
 
 
+var  TableauNomArticle = new Array();// creation du tableau ou seron stocker les noms des article
+var  TableauQuantiterArticle = new Array();// creation du tableau ou seron stocker les quantiter
+var TablePrixUniter = new Array();// creation du tableau ou seron stocker les prix a l'uniter
+var TablePrixLot = new Array();// creation du tableau ou seron stocker lex prix au lot
+var PrixTotal = 0; // initialisation de la variable PrixTotal
+
+
+function InsertionValeur(){ // creation de la fonction qui va gerer l'insertion de valeur
+
+
+    var DemandeInsertionValeur = prompt("voulez vous insrer des nouvelles valeurs (Oui/Non) : "); // demande d'insertion d'une nouvelle valeur
+    
+
+    if (DemandeInsertionValeur = "Oui") {
+        var IdArticle = 0; // initialisation de la variable IDArticle
+        var Indice = 0 // initialisation de la variable Indice
+        var NombreArticle = parseInt(prompt("Combient d'article voulez vous inserer"));// demande du nombre d'article   crée
+        
+        for (Indice; Indice < NombreArticle;Indice++){ // Tant que l'indice est plus petit que le nombres d'article que l'on veut entrer on repete la boucle de saisie et  on compte le nombre d'artilce
+            IdArticle = IdArticle+1;
+            var NomArticle = prompt("Entrer le nom de l'article n°"+IdArticle);//saisie du nom de l'article
+            var QuantiterArticle = parseFloat(prompt("Entrer la quantier de l'article  n°"+IdArticle))//saisie de la quatiter d'article
+            var PrixUniter = parseFloat(prompt("Entrer le prix a l'uniter de l'article n°"+IdArticle))//saisie du prixs a l'uniter
+            var PrixLot = (QuantiterArticle*PrixUniter);  //Calcul du pix du lot 
+            PrixTotal = PrixTotal+PrixLot; // calcul du prix total (somme des prix du lot)
+                          
+                          
+
+
+            TableauNomArticle.push(NomArticle+"<br>"); // ajout du  nom de l'article dans le taleau
+            TableauQuantiterArticle.push(QuantiterArticle+"<br>");// ajout de la quantiter de l'article
+            TablePrixUniter.push(PrixUniter+"€"+"<br>");// ajout des prix a l'uniter dans le tableau
+            TablePrixLot.push(PrixLot+"€"+"<br>");// ajout des prix des lot dans le tableau
+        
+        }
+
+        
+
+    }
+
+    else {
+            
+            if ( DemandeInsertionValeur === "Non") {                                            
+                document.write("non");
+            }
+            
+            else{
+                document.write("Saisie incorecte");
+            }
+        }
+    
+
+    
+
+    }
+
+   
+
+function CalculerValeurs(){  //creation de la fonction qui va venir afficher les valeurs 
+    
+    var TableauNomArticleString = TableauNomArticle.join(""); // coversio du tableau en chaine de caractère pour suprimer la virgule a l'affichage
+    var TableauQuantiterString = TableauQuantiterArticle.join("");// coversion du tableau en chaine de caractère pour suprimer la virgule a l'affichage
+    var TablePrixUniterString = TablePrixUniter.join("");// coversion du tableau en chaine de caractère pour suprimer la virgule a l'affichage
+    var TablePrixLotString = TablePrixLot.join("");// coversion du tableau en chaine de caractère pour suprimer la virgule a l'affichage
+    document.getElementById("ZoneAffichageNomArticle").innerHTML=TableauNomArticleString; //Afficher letableau dans la zone identifier ici ZoneAficchageX
+    document.getElementById("ZoneAffichageQuatiter").innerHTML=TableauQuantiterString;//Afficher letableau dans la zone identifier ici ZoneAficchageX
+    document.getElementById("ZoneAffichagePrixUniter").innerHTML=TablePrixUniterString;//Afficher letableau dans la zone identifier ici ZoneAficchageX
+    document.getElementById("ZoneAffichageAgePrixLot").innerHTML=TablePrixLotString;//Afficher letableau dans la zone identifier ici ZoneAficchageX
+    document.getElementById("ZoneAffichageAgePrixTotal").innerHTML="PrixTotal:"+PrixTotal+"€";//Afficher letableau dans la zone identifier ici ZoneAficchageX
+
+    
+   
+}
--- README.md
+++ README.md
@@ -5,6 +5,18 @@ Bonjour,
 Ce fichier contient le  premier projet de creation de site personnelle  en vu de la fromation du bts SIO.
 Le controle des version est controler Par git et stocker par git hub.
 
+Dans la nav bar on peut se connecter en tant que admin avec les identifiant suivant:  
+
+Identifiant de connexion : Admin
+Mots de passe : Root
+
+
+si la nouvelle page  qui donne l'acces a la section gestion de membre et calculateur ne s'ouvre pas alors 
+il faut laisser  le navigateur ouvrir les page pop up  
+
+
+
+
 
 
 :) Rezig Rachid  Bts SIO2
